818852d4ad0a6c6d67a03326a2a7f5cb63b21482
Yuya.Nishida <yuya@j96.org>
2017-07-28 19:41:19 +0900

    Leiamsync::Loggable#d: support extra args.

--- mrblib/leiamsync/loggable.rb
+++ mrblib/leiamsync/loggable.rb
@@ -1,12 +1,22 @@
 module Leiamsync::Loggable
   class << self
     attr_accessor :logger
+
+    def format_message(message, extra = nil)
+      # Kernel#caller_locations is not defined in mruby-1.3.0.
+      a = caller(2, 1)
+      filename_and_line_number = a.first[/[\w.]+:\d+/]
+      extra_s = ""
+      if extra
+        extra_s = ": " + extra.map { |k, v|
+          "#{k}=#{v.inspect}"
+        }.join(" ")
+      end
+      return filename_and_line_number + ":" + message + extra_s
+    end
   end
 
-  def d(message)
-    # Kernel#caller_locations is not defined in mruby-1.3.0.
-    a = caller(2, 1)
-    filename_and_line_number = a.first[/[\w.]+:\d+/]
-    Leiamsync::Loggable.logger.debug(filename_and_line_number + ":" + message)
+  def d(*args)
+    Leiamsync::Loggable.logger.debug(Leiamsync::Loggable.format_message(*args))
   end
 end

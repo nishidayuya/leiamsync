b1ede848f8123786b0eb8d36ed44463c74832cd1
Yuya.Nishida <yuya@j96.org>
2017-07-29 16:42:20 +0900

    split off hack code.

---
+++ mrblib/leiamsync/_.rb
@@ -0,0 +1,7 @@
+module Leiamsync
+  # If remove this empty definition, mruby-1.3.0 raises NameError.
+end
+
+module Leiamsync::Loggable
+  # If remove this empty definition, mruby-1.3.0 raises NameError.
+end
--- mrblib/leiamsync/cli.rb
+++ mrblib/leiamsync/cli.rb
@@ -1,11 +1,3 @@
-module Leiamsync
-  # If remove this empty definition, mruby-1.3.0 raises NameError.
-end
-
-module Leiamsync::Loggable
-  # If remove this empty definition, mruby-1.3.0 raises NameError.
-end
-
 class Leiamsync::Cli
   include Leiamsync::Loggable
 
